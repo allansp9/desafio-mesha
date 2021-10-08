737e64a2f619cbf94ae3820e100aeb497aa25a85
allan <allansp9@gmail.com>
2021-10-08 16:39:48 -0300

    feat: add the necessary code for the api calls

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,86 @@
-import Head from 'next/head'
+import axios from "axios";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import moment from "moment";
 
-export default function Home() {
+export default function Home({ genres }) {
+  const [city, setCity] = useState("");
+  const [cityInfo, setCityInfo] = useState(null);
+  const [playlist, setPlaylist] = useState(null);
+  const [temp, setTemp] = useState(null);
+  const [genre, setGenre] = useState(null);
+  const [timestamp, setTimestamp] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  function changeHandler(e) {
+    if (e.target.value.trim() !== "") {
+      const trimmedCity = e.target.value.trim();
+      setCity(trimmedCity);
+    }
+  }
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    if (city.trim() !== "") {
+      try {
+        const response = await axios.get(
+          encodeURI(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=73550798a02d06973c60723797d6ed96&units=metric`
+          )
+        );
+        if (response.data) {
+          setTimestamp(moment.unix(response.data.dt).format("D/M/YYYY"));
+          setCityInfo(response.data);
+          console.log(response.data);
+          setTemp(parseInt(response.data.main.temp));
+
+          let genreFilter = "";
+          if (temp > 32) {
+            genreFilter = "genre-global-chart-7";
+            setGenre("Rock");
+          }
+          if (temp < 32 && temp > 24) {
+            genreFilter = "genre-global-chart-1";
+            setGenre("Pop");
+          }
+          if (temp < 24 && temp > 16) {
+            genreFilter = "genre-global-chart-10";
+            setGenre("Country");
+          }
+          if (temp < 16) {
+            genreFilter = "genre-global-chart-11";
+            setGenre("AfroBeats");
+          }
+
+          try {
+            const options = {
+              method: "GET",
+              url: "https://shazam.p.rapidapi.com/charts/track",
+              params: {
+                locale: "en-US",
+                listId: genreFilter,
+                pageSize: "20",
+                startFrom: "0",
+              },
+              headers: {
+                "x-rapidapi-host": "shazam.p.rapidapi.com",
+                "x-rapidapi-key": process.env.NEXT_PUBLIC_SHAZAM_KEY,
+              },
+            };
+            const response = await axios.request(options);
+            console.log(response.data);
+            setPlaylist(response.data.tracks);
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      setIsLoaded(true);
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -8,75 +88,53 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <form>
+        <input
+          type="text"
+          name="city"
+          value={city}
+          onChange={(e) => changeHandler(e)}
+          placeholder="Ex: Tokyo"
+          autoComplete="off"
+        />
+        <button onClick={(e) => submitHandler(e)}>search</button>
+      </form>
+      <div>
+        {isLoaded && playlist && (
+          <ul>
+            {playlist.map((track) => (
+              <li key={track.key}>{track.title}</li>
+            ))}
+          </ul>
+        )}
+        {isLoaded && !playlist && <p>City not found</p>}
+      </div>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const options = {
+    method: "GET",
+    url: "https://shazam.p.rapidapi.com/charts/list",
+    headers: {
+      "x-rapidapi-host": "shazam.p.rapidapi.com",
+      "x-rapidapi-key": process.env.NEXT_PUBLIC_SHAZAM_KEY,
+    },
+  };
+
+  try {
+    const response = await axios.request(options);
+    if (response.data) {
+      return {
+        props: {
+          genres: response.data.global.genres,
+        },
+      };
+    }
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
 }
